d7159d9378b5bc32405a1eadfd29390be1ee8caf
Omar Villada <olvillada@gmail.com>
2021-07-02 12:56:11 -0500

    Se agregan comentarios

--- src/main/java/PasswordUtil.java
+++ src/main/java/PasswordUtil.java
@@ -3,12 +3,15 @@ public class PasswordUtil {
         WEAK,MEDIUM,STRONG
     }
     public  static SecurityLevel assessPassword(String password){
+        // si es menor a 8 caracteres es debil
         if(password.length() < 8) {
             return SecurityLevel.WEAK;
         }
+        //Si contiene solo letras
         if (password.matches("[a-zA-Z]+")){
             return SecurityLevel.WEAK;
         }
+        //Si contiene letras y numeros
         if (password.matches("[a-zA-Z0-9]+")) {
             return SecurityLevel.MEDIUM;
         }
--- src/test/java/PasswordUtilTest.java
+++ src/test/java/PasswordUtilTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class PasswordUtilTest {
     @Test
     public void weak_when_has_less_than_8_letters(){
