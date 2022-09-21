481968af5f5fbd9b59f45fa5b5012b1146182b4f
CryptoX-504 <olvillada@gmail.com>
2022-09-21 16:06:26 -0500

    Test unitario con JUnit

--- src/test/java/com/example/javatests/util/StringUtilTest.java
+++ src/test/java/com/example/javatests/util/StringUtilTest.java
@@ -1,18 +1,28 @@
 package com.example.javatests.util;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class StringUtilTest {
 
-    public static void main(String[] args) {
-        String result = StringUtil.repeat("hola", 3);
-        if (!result.equals("holaholahola")){
-            System.out.println("ERROR");
-        }
+    @Test
+    public void repeat_string_once(){
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
 
-        String result1 = StringUtil.repeat("hola", 1);
-        if (!result1.equals("hola")){
-            System.out.println("ERROR");
-        }
+    @Test
+    public void repeat_string_multiple_times(){
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times(){
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times(){
+        StringUtil.repeat("hola", -1);
     }
 }
